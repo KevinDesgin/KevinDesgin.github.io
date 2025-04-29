3aedae2fafa7db33c6f4e2cefa561b048d31dfb3
Kevin <162404830+KevinDesgin@users.noreply.github.com>
2025-04-29 20:45:43 +1000

    A2 change3

--- assignment2/script.js
+++ assignment2/script.js
@@ -31,6 +31,10 @@ function updateProgressBar() {
 
 // Add other functionalities here
 
+// Mute or Unmute Function:
+// I have employed a working mute toggle that when clicked would disable the sound of the video. As well as clciking it again would enable the sound back on.
+// This is done with the true and false
+// This way, the viewer are able to control when sound is played on this media player.
 function toggleSound() {
   if (video.muted) {
     video.muted = false;
@@ -41,6 +45,9 @@ function toggleSound() {
   }
 }
 
+//Loop Function
+// I have employed a working loop toggle that when clcicked would enable the ability to repeat the video when the video reaches to the end.
+// Giving a way to
 function toggleLoop() {
   if (video.loop) {
     video.loop = false;
@@ -49,14 +56,17 @@ function toggleLoop() {
   }
 }
 
+//Fast foward 30sec function
 function toggleFF() {
   video.currentTime += 15;
 }
 
+// Rewind 30sec function
 function toggleRW() {
   video.currentTime -= 15;
 }
 
+// Subscribe Function
 const subBtn = document.querySelector("#sub-btn");
 const subImg = document.querySelector("#sub-img");
 
