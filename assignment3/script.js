0aec086fedce157556908278d7e30f563cb7fcbf
Kevin <162404830+KevinDesgin@users.noreply.github.com>
2025-06-10 20:18:46 +1000

    3.4 Onclick

--- assignment3/script.js
+++ assignment3/script.js
@@ -1,13 +1,16 @@
+const playBtn = document.querySelector("#play");
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
+const cursor = document.querySelector(".cursor");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+// Adjustment of speed
 const startColor = "white";
 const endColor = "blue";
 let fillHeight = 0;
 const totalHeight = canvas.height;
-const animationSpeed = 0.3; // Adjust for desired speed
+const animationSpeed = 0.3;
 
 let rectX = 0;
 let rectY = 0;
@@ -15,7 +18,14 @@ const rectWidth = canvas.width;
 const rectHeight = 30;
 const fallSpeed = 0.5;
 
-function play() {
+// Play Function
+function clickPlay() {
+  document.addEventListener("mousemove", (event) => {
+    const { width, height } = cursor.getBoundingClientRect();
+    cursor.style.left = `${event.clientX - width / 2}px`;
+    cursor.style.top = `${event.clientY - height / 2}px`;
+  });
+
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   fillHeight += animationSpeed;
@@ -29,15 +39,6 @@ function play() {
   rectY += fallSpeed;
 
   if (fillHeight < totalHeight) {
-    requestAnimationFrame(play);
+    requestAnimationFrame(clickPlay);
   }
 }
-
-play();
-
-const cursor = document.querySelector(".cursor");
-document.addEventListener("mousemove", (event) => {
-  const { width, height } = cursor.getBoundingClientRect();
-  cursor.style.left = `${event.clientX - width / 2}px`;
-  cursor.style.top = `${event.clientY - height / 2}px`;
-});
